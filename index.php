3f60d8e50919a35ae5ed5f29dfd9046a65ebc23d
tianhua <azheng@SI-AZHENG.corp.valueclick.com>
2014-04-24 13:54:21 +0800

    yanhe demo

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'includes/settings.php'; ?>
+<?php include 'includes/header.php'; ?>
+
+
+    <div class="container theme-showcase">
+
+      <div class='upper-banner'><img src='assets/images/index.jpg' alt='banner' style='max-width:100%;'/></div>
+
+
+    </div> <!-- /container -->
+
+
+<?php include 'includes/footer.php';?>
